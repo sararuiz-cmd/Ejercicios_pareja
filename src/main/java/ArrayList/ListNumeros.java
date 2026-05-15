946040cb2ccfb93a7d2b16c00542026ff65fc1a7
Sara Ruiz <svruiz@uamv.edu.ni>
2026-05-15 15:58:33 -0600

    feat: agregar Clase ListNumeros

---
+++ src/main/java/ArrayList/ListNumeros.java
@@ -0,0 +1,22 @@
+package ArrayList;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListNumeros {
+    public static void main(String[] args) {
+        List<Integer> numeros=new ArrayList<>();
+        StringBuilder formato=new StringBuilder();
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(3);
+        numeros.add(4);
+        numeros.add(5);
+        for(int i:numeros){
+            formato.append(i).append("\n");
+        }
+        JOptionPane.showMessageDialog(null,"Numeros en el arreglo: \n"+formato);
+
+    }
+}
