6428b089bb905255212cbb1ac3739581f7164fae
Sara Ruiz <svruiz@uamv.edu.ni>
2026-05-15 16:00:49 -0600

    feat: agregar contenido a paquetes restantes

---
+++ src/main/java/LinkedList/Ejercicio1.java
@@ -0,0 +1,7 @@
+package LinkedList;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        System.out.println("Ejercicio 1");
+    }
+}
---
+++ src/main/java/ListOrder/Ejercicio1.java
@@ -0,0 +1,7 @@
+package ListOrder;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        System.out.println("Ejercicio1");
+    }
+}
